f986cbd1e17fffc8713b160062d2e13d03614ec6
dilarayyilmaz <zylmazdilara@gmail.com>
2025-08-06 16:59:18 +0300

    changed

--- eventure/lib/main.dart
+++ eventure/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Eventure Auth UI',
+      title: 'Eventure',
       debugShowCheckedModeBanner: false,
 
       theme: lightTheme, 
